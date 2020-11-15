801220e002d90a2e740f1ec54ef51a27f2083815
Евгений <evgenij_makarev20021@mail.ru>
2020-11-16 01:13:42 +0300

    4

--- part2/data.py
+++ part2/data.py
@@ -12,3 +12,22 @@ print(dataFrame.dtypes)
 print(dataFrame.size)
 # Кортеж, в котором хранится количество строк и столбцов
 print(dataFrame.shape)
+allList = dataFrame.shape[0]
+
+# Фильтрация данных
+menList = dataFrame.filter(like='sex').values
+mensHalf = 0
+for i in range(len(menList)):
+    if menList[i][0] == 'M':
+        mensHalf += 1
+womensHalf = allList - mensHalf
+
+# Вывод процентных соотношений
+print(mensHalf/allList*100)
+print(womensHalf/allList*100)
+
+# Процент выживших мужчин до 45 к общему числу mensHalf
+Alive = 'A'
+Age = '45'
+menQuery = dataFrame.query('status == @Alive').query('age < @Age').shape[0]
+print(menQuery/allList*100)
